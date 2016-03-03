f888602d10e66af8cffdb613cf6d67835092c9b6
Wendy <ckky123@gmail.com>
2016-03-03 09:20:48 +0800

    update practice

--- Practice/practice6.php
+++ Practice/practice6.php
@@ -1,8 +1,15 @@
 <html>
     <body>
         <?php
-        funciton Isarray($a,$b)
-        
+        $numbers=array(8,2,3,4,5,9,0);
+        $names=array("mark","Rich","Rosie","Savy","Ben");
+        arsort($numbers);
+        foreach($numbers as $num){
+        echo $num."\n";}
+        echo"<br>";
+        foreach($names as $name){
+        echo $name;    
+        }
         ?>
     </body>
 </html>
